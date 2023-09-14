3133ccaf3e090ebde4e98f32448c876f3db50e01
Acidion <acidburn947@gmail.com>
2023-09-14 16:04:47 -0500

    Reconfigured json
    I'm dumb, put content instead of name

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ var statusCheck = new CronJob(config.cronStatus,async function () {
     const statusURL = config.statusPostURL;
 
     const postData = JSON.stringify({
-        'content': config.botName, 'time' : Date.now()
+        'name': config.botName, 'time' : Date.now()
     });
 
     const options ={
